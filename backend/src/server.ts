f0da7a4f8ceeb1f235cb1a0e6baca8080ae063fe
YuriMont <yuri.mont1043@gmail.com>
2023-03-14 16:54:24 -0300

    backend iniciado

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import userRoutes from "./http/routes/UserRoutes";
+
+//Lembre-se de executar o prisma migrate
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/user", userRoutes);
+
+app.listen(3333, () => {
+    console.log("Server Running");
+});
